6bec66429df43ea30e6994fa22ab3d6c26185e7a
shivam <shivamrock887466@gmail.com>
2024-07-29 15:49:47 +0530

    zod_signup_schema_changed

--- backend/middleware/user/usermidlleware.js
+++ backend/middleware/user/usermidlleware.js
@@ -1,11 +1,11 @@
 const { z }=require('zod');
 
 const signin_Schema=z.object({
-    firstname:z.string(),
-    lastname:z.string(),
-    number:z.number(),
-    username:z.string(),
-    password:z.string(),
+    firstname:z.string().min(3).max(15),
+    lastname:z.string().min(3).max(15),
+    number:z.string().length(10),
+    username:z.string().min(3).max(15),
+    password:z.string().min(3).max(15),
 })
 
 const login_schema=z.object({
@@ -23,9 +23,9 @@ function sign_up_middleware(req,res,nxt){
     }
     catch{
 
-        res.json({
-            msg:"wrond validation"
-        })
+        res.json(
+            signin_Schema.safeParse(req.body) 
+        )
 
     }
 }
